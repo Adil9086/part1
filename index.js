e8965d2f0b179d3507310e9fce200f7cf4428ed1
Adil9086 <154617069+Adil9086@users.noreply.github.com>
2024-12-14 17:13:28 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+// let totalmarks = "88"
+
+// if(totalmarks >=80 && totalmarks <=89){
+//     console.log("Grade: A 80% of total marks")
+// } else if(totalmarks >=90 && totalmarks <=99){
+//     console.log("Grade: A+ 90% of total marks")
+// }
+// let previousmarks = 89;
+// let currentmarks = 97;
+
+// console.log(` previous marks ${previousmarks} current marks ${currentmarks}`);
+
+// if (currentmarks >= previousmarks) {
+//   console.log("current marks are higher than previous marks");
+// } else if (currentmarks <= previousmarks) {
+//   console.log("current marks are lower than previouzs marks");
+// }
+
+// if (currentmarks >= previousmarks) {
+//   console.log(
+//     "Percentage increase: " +
+//       ((currentmarks - previousmarks) / previousmarks) * 100 +
+//       "%"
+//   );
+// } else {
+//   console.log(
+//     "Percentage decrease: " +
+//       ((previousmarks - currentmarks) / previousmarks) * 100 +
+//       "%"
+//   );
+// }
+// let years = "2024";
+
+// console.log(Number(years));
+
+// let months = 12;
+// console.log(String(months));
+
+// let a = 1 - 2 + "3";
+
+// console.log(a);
+
+// let b = a - 9 + "4";
+// console.log(b);
+
+// console.log(-b * 2);
+// let money;
+// if (money) {
+//   console.log("do not spend all");
+// } else {
+//   console.log("get a job ");
+// }
+// let text = prompt("Please enter number of day");
+// let day = text;
+
+// switch (day) {
+//   case "1":
+//     console.log("today is monday");
+//     break;
+//   case "2":
+//     console.log("today is tuesday");
+//     break;
+//   case "3":
+//     console.log("today is wednesday");
+//     break;
+//   case "4":
+//     console.log("today is thursday");
+//     break;
+//   case "5":
+//     console.log("today is friday");
+//     break;
+//   case "6":
+//     console.log("today is saturday");
+//     break;
+//   case "7":
+//     console.log("today is sunday");
+//     break;
+//   default:
+//     console.log("invalid day");
+// }
+// let username = prompt("Please enter username");
+// let pass = prompt("Please enter password");
+// if (username == "Adil") {
+//   if (pass == "Adil") {
+//     console.log("Welcome Adil");
+//   } else {
+//     console.log("Invalid password");
+//   }
+// } else {
+//   console.log("Invalid username");
+// }
